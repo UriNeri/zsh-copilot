89540cb6d53f2684af47d4d5c833ae7c7adce88b
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-01-06 00:18:54 +0100

    Enhance version check in zsh-copilot: display current and new version numbers when an update is available

--- zsh-copilot.plugin.zsh
+++ zsh-copilot.plugin.zsh
@@ -1,7 +1,9 @@
 # Check if version is up to date from https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION
 current_folder=${0:A:h}
-if [ "$(cat ${current_folder}/VERSION)" != "$(curl -s https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION)" ]; then
-    echo "A new version of zsh-copilot is available. Would you like to update it? (y/n)"
+new_version=$(curl -s https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION)
+current_version=$(cat ${current_folder}/VERSION)
+if [ "$current_version" != "$new_version" ]; then
+    echo "A new version ($current_version -> $new_version) of zsh-copilot is available. Would you like to update it? (y/n)"
     read -r update
     if [ "$update" = "y" ]; then
         echo "Updating zsh-copilot to the latest version..."
