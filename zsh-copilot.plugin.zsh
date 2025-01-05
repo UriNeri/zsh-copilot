0fca95716d3c2bf22e6971aec95c37a252b4af85
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-01-06 00:13:54 +0100

    check version

--- zsh-copilot.plugin.zsh
+++ zsh-copilot.plugin.zsh
@@ -1,1 +1,7 @@
+# Check if version is up to date from https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION
+if [ "$(cat ${0:A:h}/VERSION)" != "$(curl -s https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION)" ]; then
+    echo "Updating zsh-copilot to the latest version..."
+    source ${0:A:h}/zsh-copilot.zsh
+fi
+
 source ${0:A:h}/zsh-copilot.zsh
