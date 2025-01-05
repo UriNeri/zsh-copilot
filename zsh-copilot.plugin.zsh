d5566eabcec8f1e527c0c5b5b31469e521db2095
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-01-06 00:19:32 +0100

    add colors

--- zsh-copilot.plugin.zsh
+++ zsh-copilot.plugin.zsh
@@ -1,12 +1,18 @@
+# Define colors
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Check if version is up to date from https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION
 current_folder=${0:A:h}
 new_version=$(curl -s https://raw.githubusercontent.com/Gamma-Software/zsh-copilot/refs/heads/master/VERSION)
 current_version=$(cat ${current_folder}/VERSION)
 if [ "$current_version" != "$new_version" ]; then
-    echo "A new version ($current_version -> $new_version) of zsh-copilot is available. Would you like to update it? (y/n)"
+    echo -e "${BLUE}A new version (${GREEN}$current_version${BLUE} -> ${GREEN}$new_version${BLUE}) of zsh-copilot is available. Would you like to update it? (y/n)${NC}"
     read -r update
     if [ "$update" = "y" ]; then
-        echo "Updating zsh-copilot to the latest version..."
+        echo -e "${BLUE}Updating zsh-copilot to the latest version...${NC}"
         zsh ${current_folder}/install.sh
     fi
 fi
